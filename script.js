b4c98d7c850dc334c21c4b52e49ccd769bc71eb5
Karen Lorin <karenl2801@nycstudents.net>
2026-01-12 23:17:46 -0500

    done

--- script.js
+++ script.js
@@ -9,27 +9,46 @@ let brown_position = 1;
 
 // Query Selectors Here
 const blueButton = document.querySelector("#blue-button");
+const pinkButton = document.querySelector("#pink-button");
+const brownButton = document.querySelector("#brown-button");
+
 const blueHorse = document.querySelector("#blue-horse");
+const pinkHorse = document.querySelector("#pink-horse");
+const brownHorse = document.querySelector("#brown-horse");
 
-
+const winnerDiv = document.querySelector("#winner");
 
 // OnClick Functions Here
 const advanceBlue = (e) => {
   blue_position += 1;
   changePosition(blueHorse, blue_position);
+  checkWinner(blue_position, "Blue");
 };
+
 const advancePink = (e) => {
-
+  pink_position += 1;
+  changePosition(pinkHorse, pink_position);
+  checkWinner(pink_position, "Pink");
 };
 
-
+const advanceBrown = (e) => {
+  brown_position += 1;
+  changePosition(brownHorse, brown_position);
+  checkWinner(brown_position, "Brown");
+};
 
 // Check for a winner
 const checkWinner = (position, color) => {
+  if (position === 5) {
+    winnerDiv.textContent = `${color} WINS! 🏆`;
 
+    blueButton.disabled = true;
+    pinkButton.disabled = true;
+    brownButton.disabled = true;
+  }
 };
-
-
 
 // Event Listeners Here
 blueButton.addEventListener("click", advanceBlue);
+pinkButton.addEventListener("click", advancePink);
+brownButton.addEventListener("click", advanceBrown);
